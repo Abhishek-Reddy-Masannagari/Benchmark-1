5954d1ceea2823cf6f50e0fd176691424e2190ef
Abhishek Reddy <abhishekreddy214@gmail.com>
2020-10-30 15:54:40 +0100

    Create data.js

---
+++ data/data.js
@@ -0,0 +1,136 @@
+
+const playlist = [{
+    id:"1",
+    name:"Vintage",
+    cover:"./assets/playlist_covers/Vintage.jpeg",
+    song:"./assets/songs/Beethoven - Für Elise.mp3",
+    songList:["Daft Punk-Get Lucky","Daft Punk-Instant Crush","Wiz Khalifa-Taylor Gang"]
+},{id:"2",
+    name:"Moon",
+    cover:"/assets/playlist_covers/Moon.jpeg",
+    song:"./assets/songs/yokohama.mp3",
+    songList:["ZHU - Good Life","Ben Böhmer-Promise","Wiz Khalifa-Secret"]
+},{id:"3",
+    name:"Sweet Gangsta",
+    cover:"./assets/playlist_covers/Sweet Gangsta .jpeg",
+    song:"./assets/songs/Kendrick Lamar - DNA.mp3",
+    songList:["Avoure - Aura","Marlboro-Prince of falls","Bones-Be a Man"]
+},{id:"4",
+    name:"Tastemakers",
+    cover:"./assets/playlist_covers/Tastemakers.jpeg",
+    song:"./assets/songs/Rich The Kid - New Freezer (Audio) ft. Kendrick Lamar-HLKYz1r23Lo.mp3",
+    songList:["Marlboro-Prince of falls","ZHU - Good Life","Daft Punk-Get Lucky"]
+},{id:"5",
+    name:"At Work",
+    cover:"./assets/playlist_covers/At Work.jpeg",
+    song:"./assets/songs/Wiz_Khalifa_So_High.mp3",
+    songList:["So High - Wiz Khalifa","Kendrick Lamar-DNA","Logic-Super Mario"]
+},{id:"6",
+    name:"Sunshine",
+    cover:"./assets/playlist_covers/Sunshine .jpeg",
+    song:"./assets/songs/everything-in-its-right-place.mp3",
+    songList:["Zhu - Fade","Eminem-Ass Like That","Eminem - Waltz"]
+},{id:"7",
+    name:"Party",
+    cover:"./assets/playlist_covers/Party.jpeg",
+    song:"./assets/songs/Kendrick Lamar - DNA.mp3",
+    songList:["Jay-z - Beach is Better", "Kanye West - Sick Fuck","Tupac-Hit'em Up"]
+},{id:"8",
+    name:"Freedom",
+    cover:"./assets/playlist_covers/Freedom .jpeg",
+    song:"./assets/songs/Kendrick Lamar - DNA.mp3",
+    songList:["Jay-z - Beach is Better", "Kanye West - Sick Fuck","Tupac-Hit'em Up"]
+},{id:"9",
+    name:"Made for you",
+    cover:"./assets/playlist_covers/Made for you.jpeg",
+    song:"./assets/songs/ZHU-Good Life.mp3",
+    songList:["Mozart-Fur Elise","ZHU-My Life","ZHU-Faded"]
+},{id:"11",
+    name:"Kitchen",
+    cover:"./assets/playlist_covers/Kitchen .jpeg",
+    song:"./assets/songs/ZHU-Good Life.mp3",
+    songList:["Bob Marley - One Love","Bob Marley - Buffalo Soldier","Logic - Get Up"]
+},{id:"12",
+    name:"French Rap",
+    cover:"./assets/playlist_covers/French Rap .jpeg",
+    song:"./assets/songs/Rag'n'Bone Man - Human (Official Video)-L3wKzyIN1yk.mp3",
+    songList:["Queen - Bohemian Rhapsody","Queen-Killer Queen","Daft Punk- Within"]
+},{id:"13",
+    name:"Soul",
+    cover:"./assets/playlist_covers/Soul.jpeg",
+    song:"./assets/songs/Beethoven - Für Elise.mp3",
+    songList:["Hippie Sabotage - I Robbed a Bank","Hippie Sabotage - Devil Eyes","Hippie Sabotage - Glimpse"]
+},{id:"14",
+    name:"Training",
+    cover:"./assets/playlist_covers/Training .jpeg",
+    song:"./assets/songs/Rag'n'Bone Man - Human (Official Video)-L3wKzyIN1yk.mp3",
+    songList:["Hustle Hard ","Kendrick Lamar -Humble","Pink Floyd-Brain Damage"]
+},{id:"15",
+    name:"Romance",
+    cover:"./assets/playlist_covers/Romance.jpeg",
+    song:"./assets/songs/everything-in-its-right-place.mp3",
+    songList:["Hippie Sabotage - I Robbed a Bank","Kendrick Lamar -Humble","Queen-Killer Queen"]
+},{id:"16",
+    name:"Drive",
+    cover:"./assets/playlist_covers/Drive .jpeg",
+    song:"./assets/songs/Beethoven - Für Elise.mp3",
+    songList:["Blue Moon-Billie Holiday","Big Spender- Peggy Lee","The Beatles-I Want to Hold You"]
+},{id:"17",
+    name:"Focus",
+    cover:"./assets/playlist_covers/Focus.jpeg",
+    song:"./assets/songs/Rag'n'Bone Man - Human (Official Video)-L3wKzyIN1yk.mp3",
+        songList:["Hippie Sabotage - I Robbed a Bank","Blue Moon-Billie Holiday","Big Spender- Peggy Lee"]
+
+}];
+
+const queen = [{
+    album:"A King of Magic",
+    cover:"https://i.pinimg.com/originals/ce/a6/ff/cea6ffddb1f9dd85200637a113467005.jpg",
+    songs:[]
+},{
+    album:"Hot Space",
+    cover:"https://i.pinimg.com/originals/e6/09/fc/e609fc4fb757791e938c22ebe4126b50.jpg",
+    songs:[]
+},{
+    album:"Sheer Heart Attack",
+    cover:"http://ecx.images-amazon.com/images/I/51efttH3xqL._SL500_.jpg",
+    songs:[]
+},{
+    album:"A Night at the Opera",
+    cover:"http://ecx.images-amazon.com/images/I/51lkhCc53PL._SL500_.jpg",
+    songs:[]
+},{
+    album:"News of the World",
+    cover:"http://ecx.images-amazon.com/images/I/41Z4WQVZEYL._SL500_.jpg",
+    songs:[]
+},{
+    album:"Queen II",
+    cover:"https://www.udiscovermusic.com/wp-content/uploads/2019/03/Queen-II-album-cover-820.jpg",
+    songs:[]
+},{
+    album:"The Game",
+    cover:"https://i.pinimg.com/originals/c4/11/5e/c4115e37d3bf2195e3a365ceeaae9cf0.jpg",
+    songs:[]
+},{
+    album:"The Works",
+    
+    cover:"https://i.pinimg.com/originals/e6/09/fc/e609fc4fb757791e938c22ebe4126b50.jpg",
+
+    songs:[]
+},{
+    album:"A Kind of Magic",
+    cover:"http://www.progarchives.com/progressive_rock_discography_covers/1755/cover_4301022102016_r.jpg",
+    songs:[]
+},{
+    album:"The Miracle",
+    cover:"https://upload.wikimedia.org/wikipedia/en/2/25/Queendeepcuts7376.jpg",
+    songs:[]
+},{
+    album:"Made in Heaven",
+    cover:"http://ecx.images-amazon.com/images/I/51a2E5xvkwL._SL500_.jpg",
+    songs:[]
+},{
+    album:"Live Killers",
+    cover:"http://ecx.images-amazon.com/images/I/51Moi0UMSVL._SL500_.jpg",
+    songs:[]
+}]
